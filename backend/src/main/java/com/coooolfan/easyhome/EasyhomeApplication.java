5093ac962ab31dc830f68fa2b70dee3f6ca311ce
YangYiFan <Coolfan1024@gmail.com>
2025-06-22 13:46:16 +0800

    init: 项目初始化

---
+++ backend/src/main/java/com/coooolfan/easyhome/EasyhomeApplication.java
@@ -0,0 +1,13 @@
+package com.coooolfan.easyhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyhomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyhomeApplication.class, args);
+    }
+
+}
