7d2fdb32bd68baa98e245c56a376d8a3c757f615
lima <zhifengmuxue@163.com>
2025-06-26 14:17:49 +0800

    feat: 增加处理后入房源库以及向量库

--- backend/src/main/java/com/coooolfan/easyhome/EasyhomeApplication.java
+++ backend/src/main/java/com/coooolfan/easyhome/EasyhomeApplication.java
@@ -2,8 +2,10 @@ package com.coooolfan.easyhome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class EasyhomeApplication {
     public static void main(String[] args) {
         SpringApplication.run(EasyhomeApplication.class, args);
--- backend/src/main/java/com/coooolfan/easyhome/service/impl/HousePublishRecordServiceImpl.java
+++ backend/src/main/java/com/coooolfan/easyhome/service/impl/HousePublishRecordServiceImpl.java
@@ -1,15 +1,22 @@
 package com.coooolfan.easyhome.service.impl;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.coooolfan.easyhome.constant.HouseConstant;
 import com.coooolfan.easyhome.constant.PublishConstant;
+import com.coooolfan.easyhome.mapper.HouseMapper;
 import com.coooolfan.easyhome.mapper.HousePublishRecordMapper;
+import com.coooolfan.easyhome.mapper.HouseVecMapper;
 import com.coooolfan.easyhome.pojo.dto.HouseDTO;
+import com.coooolfan.easyhome.pojo.entity.House;
 import com.coooolfan.easyhome.pojo.entity.HousePublishRecord;
 import com.coooolfan.easyhome.service.HousePublishRecordService;
+import jakarta.annotation.Resource;
+import lombok.val;
+import org.noear.solon.ai.embedding.EmbeddingModel;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,6 +29,15 @@ import java.util.List;
 public class HousePublishRecordServiceImpl
         extends ServiceImpl<HousePublishRecordMapper, HousePublishRecord>
         implements HousePublishRecordService {
+
+    @Resource
+    private HouseMapper houseMapper;
+
+    @Resource
+    private HouseVecMapper houseVecMapper;
+
+    @Resource
+    private EmbeddingModel embed;
 
     @Override
     public void publish(Long userId, HouseDTO dto) {
@@ -58,6 +74,7 @@ public class HousePublishRecordServiceImpl
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public void review(Long id, boolean pass, String reason) {
         HousePublishRecord record = this.getById(id);
         if (record == null) {
@@ -66,7 +83,33 @@ public class HousePublishRecordServiceImpl
 
         if (pass) {
             record.setStatus(PublishConstant.APPROVED);
-            record.setReason(null);
+            record.setReason(reason);
+            House house = House.builder()
+                    .title(record.getTitle())
+                    .address(record.getAddress())
+                    .area(record.getArea())
+                    .floor(record.getFloor())
+                    .image(record.getImage())
+                    .price(record.getPrice())
+                    .rooms(record.getRooms())
+                    .buildYear(record.getBuildYear())
+                    .decoration(record.getDecoration())
+                    .orientation(record.getOrientation())
+                    .unitPrice(record.getUnitPrice())
+                    .build();
+
+            try {
+                houseMapper.insert(house);
+                val embedding = embed.input(house
+                                .toString()).call()
+                        .getData().getFirst()
+                        .getEmbedding();
+                houseVecMapper.insertHouseVec(house.getId(), Arrays.toString(embedding));
+
+            }catch (Exception e) {
+                log.error(HouseConstant.FAIL_ADD);
+                throw new RuntimeException(e);
+            }
         } else {
             record.setStatus(PublishConstant.REJECTED);
             record.setReason(reason);
